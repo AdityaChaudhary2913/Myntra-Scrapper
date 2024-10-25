c10f4b41d8d64f57be13e60da0a815c8181529d6
AdityaTheJaat <programmingID1306@gamil.com>
2024-10-25 22:08:07 +0530

    MongoDB connected

---
+++ src/constants/__ini__.py
@@ -0,0 +1,5 @@
+MONGODB_URL_KEY: str = "mongodb+srv://aditya1306:aditya1306@phishingclassifier.y2fj1x3.mongodb.net/?retryWrites=true&w=majority&appName=PhishingClassifier"
+MONGO_DATABASE_NAME: str = "myntra_scrapper"
+
+
+SESSION_PRODUCT_KEY: str = "product_name"
