fe4796b168e4424f192fe30563663cc727802b89
AdityaTheJaat <programmingID1306@gamil.com>
2024-10-25 18:29:42 +0530

    Project Setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Myntra Scrapper',
+    version='0.0.1',
+    author='Aditya Chaudhary',
+    author_email='adityachaudhary1306@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
